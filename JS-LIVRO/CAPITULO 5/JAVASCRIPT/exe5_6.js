4f619ea8d275811531eb87fccfeb3d80f765d79a
Álife Silva De Moraes <alifesilva90@gmail.com>
2022-05-03 10:06:41 -0300

    UPDATE 03-05-2022

--- JS-LIVRO/CAPITULO 5/JAVASCRIPT/exe5_6.js
+++ JS-LIVRO/CAPITULO 5/JAVASCRIPT/exe5_6.js
@@ -4,7 +4,10 @@ btn_Add.addEventListener('click', adicionar_Numero)
 let btn_Check_order = document.querySelector('#btn_Check_Order')
 btn_Check_order.addEventListener('click', checar_Ordem)
 
+let btn_Reload = document.querySelector('#btn_reload')
+btn_Reload.addEventListener('click', recarregar)
 let number_array = []
+let erros = []
 
 function adicionar_Numero() {
   let in_Number = document.querySelector('#in_Number')
@@ -14,11 +17,18 @@ function adicionar_Numero() {
     alert('Informe os dados corretamente')
     location.reload()
     in_Number.focus()
+  } else {
+    number_array.push(number)
+    outList.textContent = number_array
+    if (erros.indexOf(number) >= 0) {
+      alert('Dados duplicados')
+      location.reload()
+    } else {
+      erros.push(number)
+      in_Number.value = ''
+      in_Number.focus()
+    }
   }
-  number_array.push(number)
-  outList.textContent = number_array
-  in_Number.value = ''
-  in_Number.focus()
 }
 
 function checar_Ordem(adicionar_Numero) {
@@ -30,11 +40,34 @@ function checar_Ordem(adicionar_Numero) {
   let outList2 = document.querySelector('#outList2')
   let order = number_array.sort((a, b) => {
     return a - b
-  })
+  }) // retorna um array
+  for (let i = 0; i < erros.length; i++) {
+    for (let j = 0; j < order.length; j++) {
+      if (erros[i] == order[j]) {
+        alert('Elementos estão crescentes')
+        outList2.innerHTML = 'Elementos:' + erros
+        btn_Add.disabled = true
+        btn_Check_order.disabled = true
+        btn_Add.className = 'oculta'
+        btn_Check_order.className = 'oculta'
+        btn_Reload.className = 'exibe'
+        btn_Reload.className = 'button'
+      } else {
+        alert('Elementos não estão crescentes')
+        location.reload()
+      }
+      break
+    }
+    break
+  }
+
   /*  if (order == false) {
     outList2.textContent = 'Os elementos não estão ordenados'
     outList.textContent = 'Elementos ordenados ' + order
   } else {
     outList2.textContent = 'Os elementos ja estavam ordenados ' + order
   } */
+}
+function recarregar() {
+  location.reload()
 }
